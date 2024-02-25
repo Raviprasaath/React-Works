033c0f9c7c3e9fff0a6918a430a29503d687ccbc
Raviprasaath <raviprasaath1@gmail.com>
2024-02-25 06:14:16 +0530

    02-25-2024

--- InputSwap-ins2/src/App.jsx
+++ InputSwap-ins2/src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 
 // rendering list of items
+// code from code sandbox
 
 export default function App() {
   const [input, setInput] = useState("");
